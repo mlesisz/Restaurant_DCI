89058b3b3581befbf9f27f9b771402e07060a2c4
mlesisz <47570129+mlesisz@users.noreply.github.com>
2021-04-06 17:54:18 +0200

    Aktualizacja Jquery
    -aktualizacja JQuery
    -Utworzenie kontrolera UserController
    - przeniesienie do niego akcji
    - Zmiana przeglądania menu na asynchroniczne

--- Restaurant_DCI/Controllers/HomeController.cs
+++ Restaurant_DCI/Controllers/HomeController.cs
@@ -6,19 +6,10 @@ namespace Restaurant_DCI.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly DB_Entities _db = new DB_Entities();
         // GET: Home
         public ActionResult Index()
         {
-            return RedirectToAction("Home");
-        }
-
-        public ActionResult Home(string category= "Dania główne")
-        {
-            Product product = new Product();
-            product.Category = category;
-            ViewBag.category = category;
-            return View(new BrowsingMenuContex(product, _db).FindProducts());
+            return RedirectToAction("Home","User");
         }
 
         
---
+++ Restaurant_DCI/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Restaurant_DCI.Contex;
+using Restaurant_DCI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Restaurant_DCI.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly DB_Entities _db = new DB_Entities();
+        public ActionResult Home(string category = "Dania główne")
+        {
+            Product product = new Product();
+            product.Category = category;
+            ViewBag.category = category;
+            return View(new BrowsingMenuContex(product, _db).FindProducts());
+        }
+        [HttpPost]
+        public ActionResult ViewCategory(string category = "Dania główne")
+        {
+            Product product = new Product();
+            product.Category = category;
+            ViewBag.category = category;
+            return PartialView("_Products", new BrowsingMenuContex(product, _db).FindProducts());
+        }
+    }
+}
